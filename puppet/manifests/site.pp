83ccef6b8827e37a9facfe3b41612270e5e5b8c5
ericstone57 <ericstone.dev@gmail.com>
2013-09-16 14:59:23 +0800

    invoke git and redid for env build

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,13 +1,15 @@
 import 'settings'
 
-Exec { path => "/usr/sbin/:/sbin:/usr/bin:/bin" }
+Exec { path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" }
 File { owner => $user, group => $group }
 
 node 'default' {
   include apt
+  include git
   include nginx
   include php
   include memcache
   include mysql
   include drush
+  include redis
 }
