69f27e9f0e596381c9bf479682978ca670752a17
ericstone57 <ericstone.dev@gmail.com>
2013-09-30 17:36:32 +0800

    * removed 163 mirror, it was not such stable recently * add "require" relationship before install python-software-properties

--- puppet/modules/apt/manifests/init.pp
+++ puppet/modules/apt/manifests/init.pp
@@ -2,16 +2,17 @@ class apt {
   # for PPA
   package {
     'python-software-properties':
-      ensure => present
+      ensure  => present,
+      require => Exec['apt-update']
   }
-  # use cn.archive.ubuntu.com mirror, faster in China
+  # use new mirror, faster in China
   file {
     '/etc/apt/sources.list':
       source => 'puppet:///modules/apt/sources.list',
   }
   exec {
     'apt-update':
-      command => 'aptitude -y update && touch /root/.apt-update',
+      command => 'aptitude -y update && aptitude -y upgrade && touch /root/.apt-update',
       creates => '/root/.apt-update',
       timeout => 900,
       require => [File['/etc/apt/sources.list']];
